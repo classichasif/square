b14cc234419724ba3717a1eb92f5d79a34734856
Nazrul Islam <mbillal.eee@gmail.com>
2023-07-13 21:30:42 +0600

    image change

--- assets/js/app.js
+++ assets/js/app.js
@@ -276,3 +276,39 @@ $(function (e) {
     ]
   })
 });
+
+// let size
+
+let sizeList = document.getElementById("size_list")
+let sizeListChild = sizeList.children
+let sizeName = document.getElementById("size_name")
+
+;[...sizeListChild].forEach((sizeItem) =>{
+  sizeItem.addEventListener("click", function(event){
+    ;[...sizeListChild].forEach(nestedSize => {
+       nestedSize.classList.remove("active")
+    });
+    this.classList.add("active")
+    let eventTarget = event.target.textContent
+    sizeName.innerText = eventTarget
+  })
+})
+
+var button = document.getElementById("addToCartBtn");
+
+setInterval(function() {
+  button.classList.add("moveBtn");
+  setTimeout(function() {
+    button.classList.remove("moveBtn");
+  }, 500);
+}, 3000);
+
+// side show
+
+let clsSlideShow = document.getElementById("cls__slideShow")
+let filterSidebar = document.getElementById("filter_sidebar")
+
+clsSlideShow.addEventListener("click", function(){
+  filterSidebar.style.left = "0px"
+})
+
